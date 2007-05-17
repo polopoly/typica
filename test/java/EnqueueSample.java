05a0cc0b1816d4aa881a8c0142e75dd43843ae63
dkavanagh <dkavanagh@7ed6c317-272d-0410-8ab0-49a3fbf2961c>
2007-05-17 22:04:20 +0000

    cleaned up sample code
    git-svn-id: http://typica.googlecode.com/svn/trunk@31 7ed6c317-272d-0410-8ab0-49a3fbf2961c

--- test/java/EnqueueSample.java
+++ test/java/EnqueueSample.java
@@ -1,49 +1,39 @@
 
-import java.io.FileInputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.rmi.RemoteException;
-import java.util.List;
-import java.util.Properties;
-
 import org.apache.log4j.Logger;
 
 import ch.inventec.Base64Coder;
 
 import com.xerox.amazonws.sqs.MessageQueue;
-import com.xerox.amazonws.sqs.QueueService;
 import com.xerox.amazonws.sqs.Message;
 import com.xerox.amazonws.sqs.SQSUtils;
 import com.xerox.amazonws.tools.LoggingConfigurator;
 
 /**
- * This sample application creates a queue with the specified name (if the queue doesn't already exist), and then sends (enqueues)
- * a message to the queue.
+ * This sample application creates a queue with the specified name (if the queue doesn't
+ * already exist), and then sends (enqueues) a message to the queue.
  */
 public class EnqueueSample {
-    private static Logger log = LoggingConfigurator.configureLogging(EnqueueSample.class);
+	private static Logger log = LoggingConfigurator.configureLogging(EnqueueSample.class);
 
-    //------------------------------------------------------------------------
-    public static void main( String[] args ) {
+	public static void main( String[] args ) {
+		final String AWSAccessKeyId = "[AWS Access Id]";
+		final String SecretAccessKey = "[AWS Secret Key]";
 
-        final String AWSAccessKeyId = "[AWS Access Id]";
-        final String SecretAccessKey = "[AWS Secret Key]";
+		try {
+			if (args.length < 2) {
+				log.error("usage: EnqueueSample <queueId> <message>");
+			}
+			String queueName = args[0];
+			String message = args[1];
 
-        try {
-            if (args.length < 2) {
-                log.error("usage: EnqueueSample <queueId> <message>");
-            }
-            String queueName = args[0];
-            String message = args[1];
+			// Create the message queue object
+			MessageQueue msgQueue = SQSUtils.connectToQueue(queueName, AWSAccessKeyId, SecretAccessKey);
+			log.info(" url returned = "+msgQueue.getUrl());
 
-            // Create the message queue object
-            MessageQueue msgQueue = SQSUtils.connectToQueue(queueName, AWSAccessKeyId, SecretAccessKey);
-            
-            String msgId = msgQueue.sendMessage( Base64Coder.encodeString(message) );
-            log.info( "Sent message with id " + msgId );
-            log.info(" url returned = "+msgQueue.getUrl());
-        } catch ( Exception ex ) {
-            log.error( "EXCEPTION", ex );
-        }
-    }
+			String msgId = msgQueue.sendMessage( Base64Coder.encodeString(message) );
+			log.info( "Sent message with id " + msgId );
+		} catch ( Exception ex ) {
+			log.error( "EXCEPTION", ex );
+		}
+	}
 }
