eb48924a5c4b26ef188e04ee2cd033ed0db3d4eb
dkavanagh <dkavanagh@7ed6c317-272d-0410-8ab0-49a3fbf2961c>
2007-10-02 18:11:13 +0000

    change from caching marshaller/unmarshallers to caching only the jaxb context objs. just as fast, less to cache, thread-safe
    git-svn-id: http://typica.googlecode.com/svn/trunk@83 7ed6c317-272d-0410-8ab0-49a3fbf2961c

--- java/com/xerox/amazonws/common/JAXBuddy.java
+++ java/com/xerox/amazonws/common/JAXBuddy.java
@@ -35,9 +35,15 @@ import javax.xml.bind.Unmarshaller;
  * @author developer@dotech.com
  */
 public class JAXBuddy {
-	public static Hashtable<Class, Marshaller> marshallerCache = new Hashtable<Class, Marshaller>();
-	public static Hashtable<Class, Unmarshaller> unmarshallerCache = new Hashtable<Class, Unmarshaller>();
+	public static Hashtable<String, JAXBContext> contextCache = new Hashtable<String, JAXBContext>();
 
+	/**
+	 * A convenience method to turn an object into a stream of XML.
+	 *
+	 * @param c the class you're serializing
+	 * @param object the object you're serializing
+	 * @return an input stream to read the XML from
+	 */
     public static <T> InputStream serializeXMLFile(Class<T> c, Object object) 
             throws JAXBException, IOException {
         Marshaller m = getMarshaller(c);
@@ -47,6 +53,13 @@ public class JAXBuddy {
         return bais;
     }
 
+	/**
+	 * A convenience method to turn an object into a string of XML.
+	 *
+	 * @param c the class you're serializing
+	 * @param object the object you're serializing
+	 * @return a string containing the XML
+	 */
     public static <T> String serializeXMLString(Class<T> c, Object object) 
             throws JAXBException, IOException {
         Marshaller m = getMarshaller(c);
@@ -55,31 +68,48 @@ public class JAXBuddy {
 		return new String(baos.toByteArray());
     }
  
+	/**
+	 * A convenience method to turn XML in a stream into an object.
+	 *
+	 * @param c the class you're deserializing
+	 * @param is the stream to read the XMl from 
+	 * @return an object representing the data from the stream
+	 */
     public static <T> T deserializeXMLStream(Class<T> c, InputStream is) throws JAXBException {
         Unmarshaller u = getUnmarshaller(c);
         T result = c.cast(u.unmarshal(is));
         return result;
     }
+
+	/**
+	 * This method will clear the internal cache we use to speed up these util functions.
+	 *
+	 * Do we think anybody will need this method? no...
+	 * Are we providing it to be nice to the anal retentive amongst us? yes.
+	 */
+	public static void clearCache() {
+		contextCache.clear();
+	}
     
     private static Marshaller getMarshaller(Class<?> c) throws JAXBException {
-		Marshaller m = marshallerCache.get(c);
-		if (m == null) {
-        	String typePackage = c.getPackage().getName();
-        	JAXBContext jc = JAXBContext.newInstance(typePackage);
-        	m = jc.createMarshaller();
-			marshallerCache.put(c, m);
+       	String typePackage = c.getPackage().getName();
+		JAXBContext jc = contextCache.get(typePackage);
+		if (jc == null) {
+        	jc = JAXBContext.newInstance(typePackage);
+			contextCache.put(typePackage, jc);
 		}
+		Marshaller m = jc.createMarshaller();
         return m;
     }
 
     private static Unmarshaller getUnmarshaller(Class<?> c) throws JAXBException {
-		Unmarshaller u = unmarshallerCache.get(c);
-		if (u == null) {
-			String typePackage = c.getPackage().getName();
-			JAXBContext jc = JAXBContext.newInstance(typePackage, c.getClassLoader());
-			u = jc.createUnmarshaller();
-			unmarshallerCache.put(c, u);
+		String typePackage = c.getPackage().getName();
+		JAXBContext jc = contextCache.get(typePackage);
+		if (jc == null) {
+			jc = JAXBContext.newInstance(typePackage, c.getClassLoader());
+			contextCache.put(typePackage, jc);
 		}
+		Unmarshaller u = jc.createUnmarshaller();
         return u;
     }
 }
